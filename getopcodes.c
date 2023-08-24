33353c547112d8829b8a6d7f09d342caf4196a7d
zwambui <wambuinganga.wn@gmail.com>
2023-08-24 03:18:29 -0700

    Stacks and Queues

--- getopcodes.c
+++ getopcodes.c
@@ -17,16 +17,18 @@ void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 		{"swap", _swap},
 		{"add", _add},
 		{"nop", _nop},
-		/*{"sub", _sub},
-		{"div", _div},
-		{"mul", _mul},
-		{"mod", _mod},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
-		{"rotl", _rotl},
-		{"rotr", _rotr},
-		{"stack", _stack},
-		{"queue", _queue},*/
+		/**
+		*{"sub", _sub},
+		*{"div", _div},
+		*{"mul", _mul},
+		*{"mod", _mod},
+		*{"pchar", _pchar},
+		*{"pstr", _pstr},
+		*{"rotl", _rotl},
+		*{"rotr", _rotr},
+		*{"stack", _stack},
+		*{"queue", _queue},
+		*/
 		{NULL, NULL}
 	};
 	int i;
